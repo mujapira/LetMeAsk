6fc5ce7ebb3c1dc8111b72d2a65c5817983af0a2
Maurício <malmasmello2@gmail.com>
2022-01-09 14:57:40 -0300

    Estipulando autorização

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,15 @@ import googleIconImage from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useAuth } from "../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
+import { ref, child, get } from "firebase/database";
 
 export function Home() {
   let navigate = useNavigate();
   const { signInWithGoogle, user } = useAuth();
   const auth = getAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
@@ -20,23 +24,22 @@ export function Home() {
 
     signInWithPopup(auth, provider).then((result) => {
       navigate("/rooms/new");
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // The signed-in user info.
-      // const user = result.user;
-      // ...
-      //  }).catch((error) => {
-      // Handle Errors here.
-      //    const errorCode = error.code;
-      //    const errorMessage = error.message;
-      //    // The email of the user's account used.
-      //    const email = error.email;
-      //    // The AuthCredential type that was used.
-      //    const credential = GoogleAuthProvider.credentialFromError(error);
-      //    // ...
     });
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+  
+    const roomRef = await get(ref(database, `rooms/${roomCode}`))
+    if (!roomRef.exists()) {
+      alert("Room does not exist.")
+      return
+    }
+    navigate(`/rooms/${roomCode}`)
   }
 
   return (
@@ -57,8 +60,13 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
